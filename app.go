e0cdf2a11f7ecda57b831cc9f97f543b7b11c004
un5t48l3 <contact@un5t48l3.com>
2023-03-08 04:18:36 +0300

    fix

---
+++ app.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/fatih/color"
+	"github.com/likexian/whois-go"
+)
+
+func main() {
+	// Open domains file
+	file, err := os.Open("domains.txt")
+	if err != nil {
+		fmt.Println("Failed to open file: ", err)
+		return
+	}
+	defer file.Close()
+
+	// Create output files
+	positiveFile, err := os.Create("positive.txt")
+	if err != nil {
+		fmt.Println("Failed to create positive output file: ", err)
+		return
+	}
+	defer positiveFile.Close()
+
+	negativeFile, err := os.Create("negative.txt")
+	if err != nil {
+		fmt.Println("Failed to create negative output file: ", err)
+		return
+	}
+	defer negativeFile.Close()
+
+	// Scan domains file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		domain := strings.TrimSpace(scanner.Text())
+
+		// Skip empty lines
+		if domain == "" {
+			continue
+		}
+
+		// Check if domain is registered
+		if isRegistered(domain) {
+			fmt.Fprintln(negativeFile, domain)
+		} else {
+			fmt.Fprintln(positiveFile, domain)
+
+			// Print unregistered domains to console in green color
+			green := color.New(color.FgGreen).SprintFunc()
+			fmt.Printf("Unregistered domain found: %s\n", green(domain))
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Failed to read file: ", err)
+		return
+	}
+
+	fmt.Println("Done!")
+}
+
+// Check if domain is registered
+func isRegistered(domain string) bool {
+	raw, err := whois.Whois(domain)
+	if err != nil {
+		return false
+	}
+	if strings.Contains(raw, "No match for domain") || strings.Contains(raw, "NOT FOUND") || strings.Contains(raw, "Status: AVAILABLE") {
+		return false
+	}
+	return true
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module example.com/m
+
+go 1.20
+
+require (
+	github.com/likexian/whois v1.14.5
+	github.com/likexian/whois-parser v1.24.4
+)
+
+require (
+	github.com/likexian/gokit v0.25.10 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+)
